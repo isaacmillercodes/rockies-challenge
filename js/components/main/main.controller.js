36a62909715973bad442c78474c7e4b18d0b366c
Isaac McGaha Miller <isaacmcgahamiller@gmail.com>
2016-10-28 21:56:41 -0600

    wrote function to intialize array of teams and populated table on index.html

--- js/components/main/main.controller.js
+++ js/components/main/main.controller.js
@@ -6,12 +6,77 @@
     .module('mlbApp.components.main')
     .controller('mainController', mainController);
 
-  mainController.$inject = ['$scope', '$http'];
+  mainController.$inject = ['MainService'];
 
-  function mainController($scope, $http) {
+  function mainController(MainService) {
     /*jshint validthis: true */
-    this.greeting = 'Hello World!';
+    const vm = this;
 
+    class ResultObject {
+      constructor(name, league) {
+        this.name = name;
+        this.league = league;
+        this.totalVisits = 0;
+        this.totalVisitsOnly = 0;
+        this.totalChangesOnly = 0;
+        this.avgVisitsPerGame = 0;
+        this.avgInningFirstVisit = 0;
+      }
+    }
+
+    vm.resultsArray = [new ResultObject('Major League Baseball', 'MLB'), new ResultObject('American League', 'AL'), new ResultObject('National League', 'NL')];
+
+    initTeams(vm.resultsArray);
+
+
+
+    //helper functions
+
+    function initTeams(array) {
+      MainService.getGamesForDay('10', '02')
+      .then(results => {
+        let gamesArray = results.data.data.games.game;
+        if (gamesArray.length) {
+          gamesArray.forEach(game => {
+
+            let leagueInfo = game.league;
+
+            let teamLeague;
+
+            if (leagueInfo[0] === 'A') {
+              teamLeague = 'AL';
+            } else {
+              teamLeague = 'NL';
+            }
+
+            if (leagueInfo[1] === 'A') {
+              teamLeague = 'AL';
+            } else {
+              teamLeague = 'NL';
+            }
+
+            let matchingHomeTeam = array.filter(team => {
+              return team.name === game.home_team_name;
+            })[0];
+
+            let matchingAwayTeam = array.filter(team => {
+              return team.name === game.away_team_name;
+            })[0];
+
+            if (!matchingHomeTeam) {
+              array.push(new ResultObject(game.home_team_name, teamLeague));
+            }
+
+            if (!matchingAwayTeam) {
+              array.push(new ResultObject(game.away_team_name, teamLeague));
+            }
+            
+          });
+        }
+        return array;
+      });
+
+    }
   }
 
 })();
--- js/components/main/main.service.js
+++ js/components/main/main.service.js
@@ -8,14 +8,32 @@
 
   MainService.$inject = ['$http'];
 
-  function MainService($scope, $http) {
+  function MainService($http) {
     /*jshint validthis: true */
     let baseUrl ='http://gd2.mlb.com/';
     let calendarUrl ='http://gd2.mlb.com/components/game/mlb/year_2016/';
 
-    $http.get(`${calendarUrl}month_05/day_09/miniscoreboard.json`).then(res => {
-      console.log(res);
-    });
+    // class ResultObject {
+    //   constructor(name, league) {
+    //     this.name = name;
+    //     this.league = league;
+    //     this.totalVisits = 0;
+    //     this.totalVisitsOnly = 0;
+    //     this.totalChangesOnly = 0;
+    //     this.avgVisitsPerGame = 0;
+    //     this.avgInningFirstVisit = 0;
+    //   }
+    // }
+    //
+    // const resultsArray = [new ResultObject('Major League Baseball', 'MLB'), new ResultObject('American League', 'AL'), new ResultObject('National League', 'NL')];
+
+    this.getGamesForDay = (month, day) => {
+      return $http.get(`${calendarUrl}month_${month}/day_${day}/miniscoreboard.json`);
+    };
+
+    this.getGameData = (gameDataUrl) => {
+      return $http.get(`${baseUrl}${gameDataUrl}/game_events.json`);
+    };
   }
 
 })();
